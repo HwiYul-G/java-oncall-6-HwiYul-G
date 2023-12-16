9dc18832b8e2aeb2d595629c0f332947efd506e5
kim <hwiyul_g@daum.net>
2023-12-16 15:04:31 +0900

    feat : 근로자 개인의 이름 검사 로직 추가

--- src/main/java/oncall/model/Employee.java
+++ src/main/java/oncall/model/Employee.java
@@ -1,7 +1,20 @@
 package oncall.model;
 
 import oncall.model.data.WorkDay;
+import oncall.util.ExceptionMessage;
 
 public record Employee(String name, WorkDay workDay) {
+    private final static int MAX_NAME_LENGTH = 5;
+
+    public Employee {
+        validateName();
+    }
+
+    private void validateName() {
+        if (name.length() > MAX_NAME_LENGTH) {
+            // TODO : error message 추후 정리
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT.getMessage());
+        }
+    }
 
 }
