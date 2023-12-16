b3992aa46538da3f9333edcdc18c4424ef03088a
kim <hwiyul_g@daum.net>
2023-12-16 17:15:42 +0900

    feat : 메인 함수 앱동작 로직 흐름 작성

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,25 @@
 package oncall;
 
+import oncall.controller.ApplicationState;
+import oncall.controller.InputController;
+import oncall.controller.OutputController;
+import oncall.model.service.BatchService;
+import oncall.view.InputView;
+import oncall.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        BatchService batchService = new BatchService();
+
+        ApplicationState applicationState = ApplicationState.getInstance();
+
+        InputController inputController = new InputController(inputView, outputView, applicationState, batchService);
+        OutputController outputController = new OutputController(inputView, outputView, applicationState, batchService);
+
+        inputController.run();
+        outputController.run();
     }
 }
