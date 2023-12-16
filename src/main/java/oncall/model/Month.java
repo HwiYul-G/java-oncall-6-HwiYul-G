107cfc6caccf4e71967287fc5a7b92ff3a4bb45a
kim <hwiyul_g@daum.net>
2023-12-16 16:03:01 +0900

    feat : 월에 따른 최대 일자 계산 기능 구현

--- src/main/java/oncall/model/Month.java
+++ src/main/java/oncall/model/Month.java
@@ -4,7 +4,8 @@ import java.util.List;
 import oncall.util.ExceptionMessage;
 
 public record Month(int month, String startDay) {
-
+    private static final List<Integer> MAX_DATE_OF_MONTH = List.of(31,28,31,30,31,30,31,31,30,31,30,31);
+    private static final int MONTH_GAP = 1;
     private static final int MIN_MONTH = 1;
     private static final int MAX_MONTH = 12;
     private static final List<String> DAYS = List.of("월", "화", "수", "목", "금", "토", "일");
@@ -26,6 +27,10 @@ public record Month(int month, String startDay) {
             // TODO : error message 추후 정리
             throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT.getMessage());
         }
+    }
+
+    public int maxDate() {
+        return MAX_DATE_OF_MONTH.get(month - MONTH_GAP);
     }
 
 }
